482d172e29307639c24f33b45c74e2c0ba8d6236
钟湘06523 <zhongxiang@navinfo.net>
2019-04-06 16:12:04 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/hello")
+public class DemoApplication {
+
+	public static void main(String[] args) throws InterruptedException {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/zz")
+	public String hello(@RequestParam(name="name") String name ,
+						@RequestParam(name="age") String age){
+		return "name= "+name+" age= "+age;
+	}
+	@GetMapping("/02")
+	public String hello02(@RequestParam(name="name") String name ,
+						  @RequestParam(name="age") String age){
+		return "name= "+name+" age= "+age;
+	}
+}
